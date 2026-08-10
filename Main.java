41fe4b632f5d3d0d82273d3b000c9a0f7d930196
Sanim Ahmed <sanimahmed26@gmail.com>
2026-08-10 12:26:39 +0100

    Tried to fix some stuff, got close

--- Main.java
+++ Main.java
@@ -13,10 +13,15 @@ public class Main {
         Book book = new Book(5000, 15000);
 
         Order o1 = createOrder(Side.BUY, converter.convert(new BigDecimal("150.00")), 7);
+        System.out.println("book: " + book);
         Order o2 = createOrder(Side.SELL, converter.convert(new BigDecimal("100.00")), 10);
-
-        book.submit(o2);
+        Order o3 = createOrder(Side.SELL, converter.convert(new BigDecimal("50.00")), 10);
+        Order o4 = createOrder(Side.BUY, converter.convert(new BigDecimal("50.00")), 10);
+        
         book.submit(o1);
+        book.submit(o2);
+        book.submit(o3);
+        book.submit(o4);
         
         System.out.println(book);
     }
--- classes/Book.java
+++ classes/Book.java
@@ -14,7 +14,7 @@ public class Book {
 
     // We can derive the idx these live in
     private long bestBidPrice = -1;
-    private long bestAskPrice = -1;
+    private long bestAskPrice = Integer.MAX_VALUE;
 
     private int sellEntries = 0;
     private int buyEntries = 0;
@@ -35,7 +35,6 @@ public class Book {
 
     public void add(Order o) {
         // Calculate offset before we cast, don't cast to int straight away as it may of corrupted already
-
         if(o.side == Side.BUY) {
             _add(o, buys);
             buyEntries++;
@@ -48,14 +47,20 @@ public class Book {
         // BUY is bid
         // SELL is ask
 
-        if(o.side == Side.BUY && o.price > bestBidPrice) bestBidPrice = o.price;
-        if(o.side == Side.SELL && o.price < bestAskPrice) bestAskPrice = o.price;
+        if(o.side == Side.BUY && o.price > bestBidPrice) {
+            bestBidPrice = o.price;
+            System.out.println("BUY");
+        }
+        if(o.side == Side.SELL && o.price < bestAskPrice) {
+            bestAskPrice = o.price;
+            System.out.println("SELL");
+        }
     }
 
     public void _add(Order o, Deque<Order>[] list) {
         long offset = o.price - pMin; 
         if(offset < 0 || offset >= list.length) throw new IllegalArgumentException("Provided order price is out of range");
-            
+
         int idx = (int)(offset);
         orderMap.put(o.id, o);
 
@@ -66,13 +71,13 @@ public class Book {
         Order o = orderMap.get(id);
         orderMap.remove(id);
 
+        System.out.println("o: " + o);
+
         int idx = (int)(o.price - pMin);
         if(o.side == Side.BUY) {
-            Deque<Order> list = buys[idx];
-            list.remove(o);
+            buys[idx].remove(o);
         } else {
-            Deque<Order> list = sells[idx];
-            list.remove(o);
+            sells[idx].remove(o);
         }
     }
 
@@ -84,43 +89,135 @@ public class Book {
 
         // Mental model: You have $300 and this person is selling for $200 then you would would't mind going and buying for $200 
         if(o.side == Side.BUY) {
-            if (bestBidPrice == -1) {
-                bestBidPrice = o.price;
-                add(o);
-                return;
-            } else if (bestAskPrice == -1) {
+            System.out.println("sellEntries: " + sellEntries);
+            if (sellEntries == 0) {
+                // No sell entries mean we won't find a match so just add straight away
+                System.out.println("o.buy getting added");
                 add(o);
                 return;
             }
             // The current bestAskPrice, we calculate its index to get the starting point
             long offset = bestAskPrice - pMin;
-            int idx = (int)(offset);
-            System.out.println("idx: " + idx);
+            int idx = (int)(offset); 
             Deque<Order> list = sells[idx];
             Order best = list.peekFirst();
 
+            if(best == null) {
+                add(o);
+                return;
+            }
+
             // Given that there is some quantity left on a matching price (matching price means o.price >= askPrice)
-            while(best.quantity >= 0) {
-                int tradeQty = Math.min(best.quantity, o.quantity);
-                o.quantity -= tradeQty;
-                best.quantity -= tradeQty;
+            while(idx < sells.length) {
+                if(best != null && o.price >= best.price) {
+                    // Two scenarios here, if best.quantity was the minimum then it would of taken it all
+                    // making it zero, else o.quantity would be zero and there is more left in the 
+                    // best.quantity which is handled by then if-branch below
+                    int tradeQty = Math.min(best.quantity, o.quantity);
+                    System.out.println("reached");
+                    o.quantity -= tradeQty;
+                    best.quantity -= tradeQty;
+                }  
+
+                System.out.println("best.quantity: " + best.quantity);
+                System.out.println("o.quantity: " + o.quantity);
+                System.out.println(best);
+                System.out.println("o.price: " + o.price);
+                System.out.println("best.price: " + best.price);
 
                 // Given that there is no more quantity left for the seller but there is more that we want to buy
                 // we need to find the next best thing which would be worser so we increment the price by idx++
                 // until we find a price that may work
                 if(best.quantity <= 0 && o.quantity > 0) {
+                    System.out.println('f');
                     // There is stuff left so we should try look for more by going upwards since we can't do any better
                     // so we need to look for something more worse but still is within the limits
 
                     // Increment idx, while it is within the book limit and until it finds a list that isn't size 0
                     // meaning there are some orders to compare with
-                    while(idx < buys.length && list.size() == 0) {
+                    System.out.println("idx: " + idx);
+                    System.out.println("sells.length: " + sells.length);
+                    System.out.println(list);
+                    while(idx <= sells.length && list.size() == 0) {
+                        System.out.println("?");
                         idx++;
                     }
-                    if(idx >= buys.length) {
+
+                    if(idx > sells.length) {
+                        add(o);
+                        return;
+                    }
+                    
+                    // Once we find one we get the list and then grab the best one there via peekFirst and check if 
+                    // it is a match, if it ain't then we would break and would need to add this to the book
+                    list = sells[idx];
+                    best = list.peekFirst();
+                    if(o.price < best.price) {
                         add(o);
                         break;
                     }
+                } else if (best.quantity == 0 && o.quantity == 0) {
+                    System.out.println("raeCHE");
+                    cancel(best.id);
+                } else if (o.quantity <= 0) {
+                    // If the order quantity ever reaches 0 or negative, then this given order has been fully fulfilled
+                    return;
+                }
+            }        
+        } else {
+            if (buyEntries == 0) {
+                add(o);
+                return;
+            }
+            // The current bestAskPrice, we calculate its index to get the starting point
+            long offset = bestBidPrice - pMin;
+            int idx = (int)(offset);
+            Deque<Order> list = buys[idx];
+            Order best = list.peekFirst();
+
+            if(best == null) {
+                add(o);
+                return;
+            }
+
+            // Given that there is some quantity left on a matching price (matching price means o.price >= askPrice)
+            while(idx >= 0) {
+                if(o.price <= best.price) {
+                    // Two scenarios here, if best.quantity was the minimum then it would of taken it all
+                    // making it zero, else o.quantity would be zero and there is more left in the 
+                    // best.quantity which is handled by then if-branch below
+                    int tradeQty = Math.min(best.quantity, o.quantity);
+                    o.quantity -= tradeQty;
+                    best.quantity -= tradeQty;
+                }  
+
+                System.out.println("best.quantity: " + best.quantity);
+                System.out.println("o.quantity: " + o.quantity);
+                System.out.println(best);
+                System.out.println("o.price: " + o.price);
+                System.out.println("ebest.price: " + best.price);
+
+                // Given that there is no more quantity left for the seller but there is more that we want to buy
+                // we need to find the next best thing which would be worser so we increment the price by idx++
+                // until we find a price that may work
+                if(best.quantity <= 0 && o.quantity > 0) {
+                    System.out.println("best: " + best);
+                    cancel(best.id);
+                    // There is stuff left so we should try look for more by going upwards since we can't do any better
+                    // so we need to look for something more worse but still is within the limits
+
+                    // Increment idx, while it is within the book limit and until it finds a list that isn't size 0
+                    // meaning there are some orders to compare with
+                    while(idx >= 0 && list.size() == 0) {
+                        idx--;
+                    }
+
+                    if(idx < 0) {
+                        add(o);
+                        System.out.println('e');
+                        return;
+                    }
+                   
                     // Once we find one we get the list and then grab the best one there via peekFirst and check if 
                     // it is a match, if it ain't then we would break and would need to add this to the book
                     list = buys[idx];
@@ -129,20 +226,14 @@ public class Book {
                         add(o);
                         break;
                     }
+                } else if (best.quantity == 0 && o.quantity == 0) {
+                    System.out.println("raeCHE");
+                    cancel(best.id);
                 } else if (o.quantity <= 0) {
                     // If the order quantity ever reaches 0 or negative, then this given order has been fully fulfilled
                     return;
                 }
             }        
-        } else {
-            if (bestAskPrice == -1) {
-                bestAskPrice = o.price;
-                add(o);
-                return;
-            } else if (bestBidPrice == -1) {
-                add(o);
-                return;
-            }
         }
 
         // If there was no match then we would add this order to the book
@@ -156,30 +247,27 @@ public class Book {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("[");
+        StringBuilder sb = new StringBuilder("BUYS: \n");
         boolean first = false;
+        System.out.println(buys.length);
         for(int i = 0; i < buys.length; i++) {
             if(buys[i].size() == 0) continue;
-            if(i != buys.length - 1 && first) sb.append(", ");
+            if(i != buys.length - 1 && first) sb.append("\n");
             first = true;
             sb.append(buys[i]);
         }
-        sb.append("]");
-        if(sellEntries == 0) {
-            return sb.toString();
-        }
-        sb.append("\n");
+        if(sellEntries == 0) return sb.toString();
+    
+        sb.append("\nSELLS: \n");
 
         first = false;
 
-        sb.append("[");
         for(int i = 0; i < sells.length; i++) {
             if(sells[i].size() == 0) continue;
-            if(i != sells.length - 1 && first) sb.append(", ");
+            if(i != sells.length - 1 && first) sb.append("\n");
             first = true;
             sb.append(sells[i]);
         }
-        sb.append("]");
         
         return sb.toString();
     }
